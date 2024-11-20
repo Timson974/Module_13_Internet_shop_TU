f26692c673a12690b8f66daeb0f7c7d6564b2cef
Timson974 <t.utepov@hotmail.com>
2024-11-20 00:27:03 +0000

    Initial commit

---
+++ backend/src/controllers/type.controller.js
@@ -0,0 +1,13 @@
+const TypeModel = require("../models/type.model");
+const TypeNormalizer = require("../normalizers/type.normalizer");
+
+class TypeController {
+    static async getTypes(req, res) {
+        let types = await TypeModel.find().populate('category');
+        console.log(types)
+        types = types.map(item => TypeNormalizer.normalize(item));
+        res.json(types);
+    }
+}
+
+module.exports = TypeController;
